b11856d39cf5b0c6f0d72e32162b895d56259b74
Brandon Amos <bdamos@vt.edu>
2013-06-13 21:18:58 -0700

    Remove minimal example code.

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "shunting-yard.h"
-
-int main() {
-  std::map<std::string, double> vars;
-  vars["pi"] = 3.14;
-  std::cout << calculator::calculate("pi+1", &vars) << std::endl;
-  return 0;
-}
