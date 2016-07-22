32d3c9053c1f51428b9fd9a3788ed40117b76a75
Vinícius Garcia <vingarcia00@gmail.com>
2016-07-21 23:55:05 -0300

    Remove `print()` tests so stdout is not polluted

--- test-shunting-yard.cpp
+++ test-shunting-yard.cpp
@@ -126,8 +126,6 @@ TEST_CASE("Function usage expressions") {
   REQUIRE_THROWS(calculator::calculate("foo(10),"));
   REQUIRE_THROWS(calculator::calculate("foo,(10)"));
 
-  REQUIRE_NOTHROW(calculator::calculate("print()"));
-
   REQUIRE(Scope::default_global()["abs"].str() == "[Function: abs]");
   REQUIRE(calculator::calculate("1,2,3,4,5").str() == "(1, 2, 3, 4, 5)");
 
