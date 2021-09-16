6e83cb1f60c70acd094d6fb1a51daeaf5d4279c0
Vishwaas-S <85667787+Vishwaas-S@users.noreply.github.com>
2021-09-16 15:25:14 +0530

    Create int.c

---
+++ int.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d", number);
+    
+    return 0;
+}
