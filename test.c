e0ede6556dde108fd7ae6832cb16992c5dd95fcb
Vishwaas-S <85667787+Vishwaas-S@users.noreply.github.com>
2022-02-08 13:38:19 +0530

    up
    up

--- test.c
+++ test.c
@@ -3,7 +3,7 @@ void f1(struct fred_t *p)
 
 
 
-    // dereference p and then check if it's NULL
+    // dereference p and then check if it's NULL undefine
     int x = p->x;
     if (p)
         do_something(x);
